52ef02794969377e6c4aff7a5fcc753a017ca41d
Kris <Valentina10@sibmail.com>
2022-06-13 00:00:51 +0700

    Creating a file fileexplorermodel.h

--- Lab_3/fileexplorermodel.h
+++ Lab_3/fileexplorermodel.h
@@ -1,4 +1,10 @@
 #ifndef FILEEXPLORERMODEL_H
 #define FILEEXPLORERMODEL_H
 
+class FileExplorerModel: public QFileSystemModel
+{
+public:
+    FileExplorerModel(QObject *parent = nullptr);
+};
+
 #endif // FILEEXPLORERMODEL_H
