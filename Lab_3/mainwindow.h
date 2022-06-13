c38e817a885535fdfc2e29eb251c4d133c6dc129
Kris <Valentina10@sibmail.com>
2022-06-13 12:07:44 +0700

    Adding private and public to mainwindow.h

--- Lab_3/mainwindow.h
+++ Lab_3/mainwindow.h
@@ -1,6 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QMainWindow>
+#include <QWidget>
+#include <QFileSystemModel>
+#include <QTreeView>
+#include <QTableView>
+
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -11,5 +18,20 @@ private slots:
     void on_checkBoxClicked();
     void on_comboBoxChanged();
     void on_paintClicked();
+
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+private:
+    QFileSystemModel *fileModel;
+    QFileSystemModel *dirModel;
+    QTreeView *treeView;
+    QTableView *tableView;
+    QtCharts::QChartView *chartView;
+    QtCharts::QChart *chart;
+    QComboBox *qbox;
+    QCheckBox *checkBox;
+    QtCharts::QValueAxis *axisY;
+};
 
 #endif // MAINWINDOW_H
